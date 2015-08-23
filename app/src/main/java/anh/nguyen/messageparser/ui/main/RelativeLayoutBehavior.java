84670a80f3e6e0f0cb679c76a4c9c95a0f2bc5be
Hoang Anh <hoanganh@siliconstraits.vn>
2015-08-23 08:39:00 +0700

    Updated UI.  Added CardView.

--- app/src/main/java/anh/nguyen/messageparser/ui/main/RelativeLayoutBehavior.java
+++ app/src/main/java/anh/nguyen/messageparser/ui/main/RelativeLayoutBehavior.java
@@ -6,22 +6,23 @@ import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import anh.nguyen.messageparser.common.ToolbarHelper;
 
 /**
  * Created by nguyenhoanganh on 8/22/15.
  */
-public class LinearLayoutBehavior extends CoordinatorLayout.Behavior<LinearLayout> {
+public class RelativeLayoutBehavior extends CoordinatorLayout.Behavior<RelativeLayout> {
     private int mToolbarHeight;
 
-    public LinearLayoutBehavior(Context context, AttributeSet attrs) {
+    public RelativeLayoutBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
         mToolbarHeight = ToolbarHelper.getToolbarHeight(context);
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, LinearLayout linearLayout, View dependency) {
+    public boolean layoutDependsOn(CoordinatorLayout parent, RelativeLayout relativeLayout, View dependency) {
         return dependency instanceof AppBarLayout;
     }
 
@@ -29,18 +30,18 @@ public class LinearLayoutBehavior extends CoordinatorLayout.Behavior<LinearLayou
      * Calculating how much of the AppBarLayout is scrolled off the screen and we are scrolling our LinearLayout accordingly
      *
      * @param parent
-     * @param linearLayout
+     * @param relativeLayout
      * @param dependency
      * @return
      */
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, LinearLayout linearLayout, View dependency) {
+    public boolean onDependentViewChanged(CoordinatorLayout parent, RelativeLayout relativeLayout, View dependency) {
         if (dependency instanceof AppBarLayout) {
-            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) linearLayout.getLayoutParams();
-            int linearLayoutBottomMargin = lp.bottomMargin;
-            int distanceToScroll = linearLayout.getHeight() + linearLayoutBottomMargin;
+            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) relativeLayout.getLayoutParams();
+            int relativeLayoutBottomMargin = lp.bottomMargin;
+            int distanceToScroll = relativeLayout.getHeight() + relativeLayoutBottomMargin;
             float ratio = dependency.getY() / (float) mToolbarHeight;
-            linearLayout.setTranslationY(-distanceToScroll * ratio);
+            relativeLayout.setTranslationY(-distanceToScroll * ratio);
         }
         return true;
     }
